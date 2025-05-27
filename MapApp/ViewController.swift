11bd03508e3ea1efd231f164527543ee73b316cc
Maksym Bailiuk <bailiukmaxym@gmail.com>
2025-05-27 11:32:32 +0200

    Initial Commit

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Максим Байлюк on 27.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
